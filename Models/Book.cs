71457405432aa2d6060052cd333e2db7d12bb76f
Haroon Iqbal <haroonsiqbal@gmail.com>
2019-11-25 13:52:25 -0600

    added books

---
+++ Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(10)]
+        [MaxLength(13)]
+        public string ISBN { get; set; }
+        
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public DateTime PublishDate { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+        public ApplicationUser Owner { get; set; } 
+
+    }
+}
